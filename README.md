e450229f48f2bff7a4431be63345e96bf688de71
trinhtungduong <sync20032001@gmail.com>
2021-12-15 11:45:25 +0700

    them giao dien qlhk

--- README.md
+++
@@ -1,7 +0,0 @@
-# NMCNPM.20211-Nhom4
-
-Authors:
-1. Trịnh Tùng Dương
-2. Lê Huy Hoàng
-3. Võ Thục Khánh Huyền
-4. Nguyễn Văn Thành
--- src/main/java/utility/Message.java
+++
@@ -1,7 +0,0 @@
-package utility;
-
-public class Message {
-    // dang nhap
-    public static String dangNhapThatBai = "Tài khoản hoặc mật khẩu không đúng!";
-    public static String dangNhapTrong = "Nhập tài khoản và mật khẩu!";
-}
